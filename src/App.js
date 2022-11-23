afa1e8755bc43f8e2b8456f9ef17ccf164bb93bf
santiago modarelli <cimspaceship@gmail.com>
2022-11-23 16:47:55 -0300

    images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Body from './components/Body';
 
+
+
 function App() {
   return (
     <div className="App">
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,43 @@
 import ItemCount from "./ItemCount"
+import {useState} from "react"
+import ItemList from "./ItemList"
 
-function ItemListContainer({greeting}) {
+
+const productos = [
+    {
+     id:"1",
+     name:"Fuente Aerocool Dorado 750W RGB",
+     description:"PSU description",
+     stock: 5,
+     price: 45000,
+     img:"https://github.com/Cimspaceship/reactjs-proyecto/blob/main/public/assets/productos/aerocool-dorado-750w-rgb.png",
+    },
+   
+    {
+     id:"2",
+     name:"Mouse",
+     description:"Mouse Description",
+     stock: 2,
+    }
+   
+   ]
+   
+
+function ItemListContainer({}) {
+
+    const [products, setProducts] = useState([])
+    
+
+    const productList = new Promise((resolve) => setTimeout(()=>{resolve(productos)},3000))
+    
+    productList.then((data)=> setProducts(data))
+
+
     return (
         <div>
-        <h1>Productos destacados</h1>
-        <h3 className="greeting">{greeting}</h3>
-        <ItemCount/>
+            <ItemList products={products}/>
         </div>
+        
     )
 }
 
