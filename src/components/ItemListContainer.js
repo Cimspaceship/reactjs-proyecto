942e831237b6ecdf026e6f034fae62f23c8d23cb
santiago modarelli <cimspaceship@gmail.com>
2022-11-22 21:32:54 -0300

    item-count

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,11 @@
+import ItemCount from "./ItemCount"
+
 function ItemListContainer({greeting}) {
     return (
         <div>
         <h1>Productos destacados</h1>
         <h3 className="greeting">{greeting}</h3>
+        <ItemCount/>
         </div>
     )
 }
