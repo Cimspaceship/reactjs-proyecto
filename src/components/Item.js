66d1a9e62d0e0c0c61ffaffab38e558fe970b446
santiago modarelli <cimspaceship@gmail.com>
2022-12-15 01:21:08 -0300

    pointer fixed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,38 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import "./Item.css"
+
+
+
+ 
+
+
 
 
 const Item = ({product}) => {
+
+  function goDetail() {
+    navigate(`/item/${product.id}`)
+  }
+
+  const navigate = useNavigate()
+
+
     return (
-        <Card style={{ width: '18rem', margin:'2rem'}}>
+      <div onClick={goDetail}>
+        <Card className='pointer' style={{ width: '18rem', margin:'2rem'}}>
         <Card.Img variant="top" src={product.img} />
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
           <Card.Text>
             ${product.price}
           </Card.Text>
-          <Link to={`/item/${product.id}`}><Button variant="primary">Añadir al Carrito</Button></Link>
         </Card.Body>
       </Card>
+    
+    </div>
     )
 }
 
